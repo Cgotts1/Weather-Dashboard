8be7a4676067f3cb38b996a9a4ccb582fbd85763
Cgotts <cobi.gottschalk@gmail.com>
2022-11-06 11:33:54 -0800

    Added styling in css, added locations for weather info in html, added current time in js

--- script.js
+++ script.js
@@ -1,15 +1,31 @@
-var btn = document.querySelector('.btn');
-var searchInput = document.querySelector('#search-input');
-var option = document.querySelector('#format-input');
-console.log(option, "options")
+var displayCurrentTime = document.querySelector("#currentTime");  // querySelector for displaying time at top of page
+var btn = document.querySelector(".btn");
+var searchInput = document.querySelector("#search-input");
+var option = document.querySelector("#format-input");
+var weatherInfo = document.querySelector(".main");
+var apiKey = "e2529e3d1d19d0acec4d2f3fc131a3ec";
+
+//Sets the date at the top of the page upon loading of the page
+setInterval(function () {
+  var time = moment().format('MMMM Do YYYY, h:mm:ss a');
+  displayCurrentTime.textContent = time;
+}, 1000);
+
+
+
+
+
+
+console.log(option, "options");
 var value = option.value;
-btn.addEventListener('click', function (event) {
+btn.addEventListener("click", function (event) {
   event.preventDefault();
-  console.log(searchInput.value)
-})
-  var RequestUrl = `https://api.openweathermap.org/data/2.5/weather?q={cityname}&appid=&appid=e2529e3d1d19d0acec4d2f3fc131a3ec`;
-  console.log(value , "value")
-var responseText = document.getElementById('response-text');
+  console.log(searchInput.value);
+});
+
+var RequestUrl = `https://api.openweathermap.org/data/2.5/forecast?q=los%20angeles&appid=e2529e3d1d19d0acec4d2f3fc131a3ec`;
+console.log(value, "value");
+var responseText = document.getElementById("response-text");
 function getApi(request) {
   fetch(request)
     .then(function (response) {
@@ -28,4 +44,50 @@ getApi(RequestUrl);
 
 
 
-`https://www.loc.gov/search/?q=${value}&fo=json`
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//api.openweathermap.org/data/2.5/forecast?q=los%20angeles&appid=e2529e3d1d19d0acec4d2f3fc131a3ec
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/* moment(data.list[i].dt_txt, 'YYYY-MM-DD HH:mm:ss').format(<insert desired format here>)
+
+plugging in your desired format in that placeholder should get you what you want
+
+moment(data.dt, "X").format("(MM/DD/YYYY)")
+This would be for current weather api, previous message is for 5-day forecast api 
+*/
