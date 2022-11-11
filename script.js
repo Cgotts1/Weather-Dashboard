e6fb9db33c48cdcaf89554279aecaea7876f9223
Cgotts <cobi.gottschalk@gmail.com>
2022-11-11 11:29:42 -0800

    Added footer tag

--- script.js
+++ script.js
@@ -20,13 +20,13 @@ $(document).ready(function(){
   $("#button").click(function(){
     event.preventDefault();
     var cityInput = $("input[name=recentCities]").val();
-    $("ol").append("<li>" + cityInput + "</li>");         //$("button#recentCities").append("<li>" + cityInput + "</li>");   button route
+    $("ol").append("<li>" + cityInput + "</li>");         
 
     // Local storage 
     localStorage.setItem(cityInput, cityInput)
     localStorage.getItem(cityInput)
-    var userCities = localStorage.getItem(cityInput)
-    userCities
+    // var userCities = localStorage.getItem(cityInput)      //probably dont need these two lines
+    // userCities
   });
 });
 
@@ -54,8 +54,8 @@ fetch(`https://api.openweathermap.org/data/2.5/forecast?q=${citySearchApi}&appid
 }
 
 
-function displayWeather(data){
-  const {city} = data;                                                            
+function displayWeather(result){
+  const {city} = result;                                                            
   // const {main} = data;
   // const {weather} = data;
 
@@ -199,194 +199,6 @@ function displayWeather(data){
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// Another fetch code, this one doesn't pull back any api data
-
-// fetch('https://api.openweathermap.org/data/2.5/forecast?q=san%20diego&appid=e2529e3d1d19d0acec4d2f3fc131a3ec')
-// .then(res => {
-//   console.log(res);
-//   return res.json();
-// })
-// .then(data =>{
-//   data.forEach(city =>{
-//     const markup = `<li>${city.name} </li>` ;
-
-//     document.querySelector('ol').insertAdjacentHTML('beforeend', markup);
-// });
-// })
-// .catch(error => console.log(error));
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// Code for local storage
-
-// let cities = " " ;
-// const addCity = (ev)=>{
-//   ev.preventDefault();
-//   let city = {
-//     cityName: document.querySelector('#savedLocations').value
-//   }
-//   cities.push(city);
-//   document.querySelector('form').reset; 
-
-
-  //display purposes
-
-
-
-  // console.warn('added' , {cities});
-  // let pre = document.querySelector('#savedLocations');
-  // pre.textContent = '\n' + JSON.stringify(cities, '\t', 2);
-
-  //Saving to local storage
-
-
-
-//   localStorage.setItem('MyCityList', JSON.stringify(cities));
-// }
-
-// document.addEventListener('DOMContentLoaded', ()=>{
-//   document.querySelector(".btn").addEventListener('click', addCity)
-// });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// console.log(option, "options");
-// btn.addEventListener("click", function (event) {
-//   event.preventDefault();
-//   console.log(searchInput.value);
-// });
-
-// var RequestUrl = `https://api.openweathermap.org/data/2.5/forecast?q=los%20angeles&appid=e2529e3d1d19d0acec4d2f3fc131a3ec`;
-// console.log(value, "value");
-// var responseText = document.getElementById("response-text");
-// function getApi(request) {
-//   fetch(request)
-//     .then(function (response) {
-//       // Check the console first to see the response.status
-//       console.log(response.status);
-//       if (response.status !== 200) {
-//         responseText.textContent = response.status;
-//       }
-//       return response.json();
-//     })
-//     .then(function (data) {
-//       console.log(data);
-//     });
-// }
-// getApi(RequestUrl);
 
 //api.openweathermap.org/data/2.5/forecast?q=los%20angeles&appid=e2529e3d1d19d0acec4d2f3fc131a3ec
 
